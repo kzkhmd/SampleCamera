56370ff47c6038bbd94e785b195d7f2ef6aef005
Kazuki Hamada <kazuki.ma9376@gmail.com>
2020-02-05 20:52:01 +0900

    Initial Commit

---
+++ SampleCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleCamera
+//
+//  Created by 濱田一輝 on 2020/02/05.
+//  Copyright © 2020 Kazuki Hamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
